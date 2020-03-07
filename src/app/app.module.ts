6f8e7a6aa85437efa5caab579d47e44c18473922
tfanelli92 <talita_0.0@hotmail.com>
2020-03-07 20:14:04 -0300

    starting cart component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,6 +9,7 @@ import { ProductListComponent } from './product-list/product-list.component';
 import { ProductAlertsComponent } from './product-alerts/product-alerts.component';
 import { ProductDetailsComponent } from './product-details/product-details.component';
 import { CartService } from './cart.service';
+import { CartComponent } from './cart/cart.component';
 
 @NgModule({
   imports: [
@@ -24,7 +25,8 @@ import { CartService } from './cart.service';
     TopBarComponent,
     ProductListComponent,
     ProductAlertsComponent,
-    ProductDetailsComponent
+    ProductDetailsComponent,
+    CartComponent
   ],
   bootstrap: [ AppComponent ],
   providers: [CartService]
--- src/app/cart.service.ts
+++ src/app/cart.service.ts
@@ -5,6 +5,21 @@ import { Injectable } from '@angular/core';
 })
 export class CartService {
 
+  items = [];
+
+  addToCart(product) {
+    this.items.push(product);
+  }
+
+  getItems() {
+    return this.items;
+  }
+
+  clearCart() {
+    this.items = [];
+    return this.items;
+  }
+  
   constructor() { }
 
 }
