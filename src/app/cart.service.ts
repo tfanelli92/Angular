caeb220f1fa766628f76eb7aff2ba4c1389311d0
tfanelli92 <talita_0.0@hotmail.com>
2020-03-08 22:11:11 -0300

    3 files modified, 4 added

--- src/app/cart.service.ts
+++ src/app/cart.service.ts
@@ -1,4 +1,5 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
@@ -19,7 +20,13 @@ export class CartService {
     this.items = [];
     return this.items;
   }
+
+    getShippingPrices() {
+    return this.http.get('/assets/shipping.json');
+  }
   
-  constructor() { }
+  constructor(
+    private http: HttpClient
+  ) {}
 
 }
